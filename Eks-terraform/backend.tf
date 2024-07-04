df112e918bdc2bd135551bc9b44f08438e09745a
WikenTeacher <wikenedu1@gmail.com>
2024-07-04 13:52:55 +0900

    update terraform

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "writetoritika" # Replace with your actual S3 bucket name
+    bucket = "henry-reddit-clone" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-northeast-2" # Replace with your actual region
   }
 }
